fc2d1dca14721e535776cc5cc4090610c27342e3
cliff12345 <108213166+cliff12345@users.noreply.github.com>
2023-02-16 21:32:25 +0530

    Add files via upload

---
+++ minecraft40lines.py
@@ -0,0 +1,41 @@
+
+#importing the libraries
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+#Adding sunset texture for game and added player and sword, added sword so that, after hovering, you can add blocks.
+app = Ursina()
+Sky(texture='sky_sunset')
+player = FirstPersonController()
+
+sword = Entity(model='assets\\blade', texture='assets\sword', rotation=(30,-40),
+               position=(0.6,-0.6), parent=camera.ui, scale=(0.2,0.15))
+
+#Math Logic for the sword position.
+def update():
+  if held_keys['left mouse']:
+    sword.position = (0.6,-0.5)
+  elif held_keys['right mouse']:
+    sword.position = (0.6,-0.5)
+  else:
+    sword.position = (0.7,-0.6)
+
+boxes = []
+for n in range(12):
+  for k in range(12):
+    box = Button(color=color.blue, model='cube', position=(k,0,n),
+                 texture='assets\grass',parent=scene, origin_y=0.5)
+    boxes.append(box)
+
+#Logic for the cube when we hover, you can click on left mouse to make a block, or press right mouse to destroy the block.
+def input(key):
+  for box in boxes:
+    if box.hovered:
+      if key == 'left mouse down':
+        new = Button(color=color.green, model='cube',position= box.position + mouse.normal,
+                     texture='assets\grass', parent=scene, origin_y=0.5)
+        boxes.append(new)
+      if key == 'right mouse down':
+        boxes.remove(box)
+        destroy(box)
+app.run()
